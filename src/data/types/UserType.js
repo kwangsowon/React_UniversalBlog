2f0063a3ef208b34c32aa7ba731ccfbd51e9b65a
yvanwangl <1012305328@qq.com>
2017-04-20 22:57:45 +0800

    modify index

--- src/data/types/UserType.js
+++ src/data/types/UserType.js
@@ -8,18 +8,18 @@
  */
 
 import {
-  GraphQLObjectType as ObjectType,
-  GraphQLID as ID,
-  GraphQLString as StringType,
-  GraphQLNonNull as NonNull,
+    GraphQLObjectType as ObjectType,
+    GraphQLID as ID,
+    GraphQLString as StringType,
+    GraphQLNonNull as NonNull,
 } from 'graphql';
 
 const UserType = new ObjectType({
-  name: 'User',
-  fields: {
-    id: { type: new NonNull(ID) },
-    email: { type: StringType },
-  },
+    name: 'User',
+    fields: {
+        id: {type: new NonNull(ID)},
+        email: {type: StringType},
+    },
 });
 
 export default UserType;
