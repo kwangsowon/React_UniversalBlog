5d6e439bf5051dd661cb5b1cdf9790c8b1f2fdc3
yvanwangl <1012305328@qq.com>
2017-04-22 23:36:14 +0800

    modify schema

--- src/data/schema.js
+++ src/data/schema.js
@@ -8,21 +8,21 @@
  */
 
 import {
-  GraphQLSchema as Schema,
-  GraphQLObjectType as ObjectType,
+    GraphQLSchema as Schema,
+    GraphQLObjectType as ObjectType,
 } from 'graphql';
 
 import me from './queries/me';
 import news from './queries/news';
 
 const schema = new Schema({
-  query: new ObjectType({
-    name: 'Query',
-    fields: {
-      me,
-      news,
-    },
-  }),
+    query: new ObjectType({
+        name: 'Query',
+        fields: {
+            me,
+            news,
+        },
+    }),
 });
 
 export default schema;
