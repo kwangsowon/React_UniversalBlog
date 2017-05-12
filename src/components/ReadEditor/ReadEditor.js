d8e8d982274db9afa91e9951f7949409ae64085d
wangyafei <wangyafei@startimes.com.cn>
2017-05-12 18:45:31 +0800

    modify wangeditor

--- src/components/ReadEditor/ReadEditor.js
+++ src/components/ReadEditor/ReadEditor.js
@@ -1,25 +1,20 @@
 import React, {Component} from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './index.css';
-import WangEditor from 'wangeditor';
 
 class ReadEditor extends Component {
     constructor(props) {
         super(props);
-		this.state = {appIsMounted: false};
     }
-
     componentDidMount () {
-		requestAnimationFrame(() => {
-			this.setState({ appIsMounted: true });
-			let id = this.props.id;
-			this.editor = new WangEditor(id);
-			this.editor.config.menus = [];
-			this.editor.create();
-			this.editor.disable();
-			// 初始化内容
-			this.editor.$txt.html(this.props.content);
-		});
+    	const WangEditor = require('wangeditor');
+    	const {id, content} = this.props;
+		this.editor = new WangEditor(id);
+		this.editor.config.menus = [];
+		this.editor.create();
+		this.editor.disable();
+		// 初始化内容
+		this.editor.$txt.html(content);
     }
 
     componentWillReceiveProps(nextProps){
@@ -27,8 +22,10 @@ class ReadEditor extends Component {
     }
 
     render() {
+    	const {content} = this.props;
         return (
             <div id={this.props.id} contentEditable="false">
+				{content}
             </div>
         );
     }
